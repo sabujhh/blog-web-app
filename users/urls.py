c708a84dd8177a25fb8bf4a617a9e58e5e58e6c7
hasan-sabuj <sabuj.hh@gmail.com>
2022-05-28 02:03:02 +0600

    first commit

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .import views
+from django.contrib.auth import views as auth_view
+
+urlpatterns = [
+    path('sign_up/', views.sign_up, name='users-sign-up'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='users-logout'),
+    path('profile/', views.profile, name='users-profile'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password_reset_done/', auth_view.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password_reset_complete/', auth_view.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
+    
+]
